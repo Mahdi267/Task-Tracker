5914084d802c71d4bd677a1a724d8197c3a46a7d
Mahdi267 <mahditzr7@gmail.com>
2026-08-23 18:46:11 -0400

    Add error handling for user creation (unique constraint)

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import { prisma } from "./prisma";
 
 const app = express();
 const PORT = 3000;
@@ -9,6 +10,26 @@ app.use(express.json());
 // Route de test
 app.get("/", (req, res) => {
     res.json({ message: "API Task Tracker en ligne !" });
+});
+
+// Route de test : créer un utilisateur
+app.post("/users", async (req, res) => {
+    const { email, password, name } = req.body;
+
+    try{
+        const user = await prisma.user.create({
+            data: { email, password, name },
+        });
+
+        res.status(201).json(user);
+    } catch (error: any) {
+        if (error.code == "P2002") {
+            res.status(409).json({ error: "Cet email est déjà utilisé." });
+        } else {
+            console.error(error);
+            res.status(500).json({ error: "Une erreur interne est survenue." });
+        }
+    }
 });
 
 app.listen(PORT, () => {
